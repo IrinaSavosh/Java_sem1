2979704726c2183b65593a78dada29f6ee53f39c
IrinaSavosh <irina_savosh@mail.ru>
2023-03-15 15:02:20 +0300

    homework 3

---
+++ hw_3.java
@@ -0,0 +1,44 @@
+package seminar_1;
+import java.util.Scanner;
+public class hw_3 {
+   public static void main (String[] args){
+      /*Реализовать простой калькулятор (+ - / *)
+Ввод числа ->
+Ввод знака ->
+Ввод числа ->*/
+      System.out.print("Введите число: ");
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+
+      System.out.print("Введите символ +, -, * или /: ");
+      String symbol = in.next(); //toString();
+
+      System.out.print("Введите число: ");
+      int m = in.nextInt();
+
+      result(n, symbol, m);
+
+      in.close();
+   }
+
+   public static void result(int one, String symbol, int two){
+      if(symbol.equals("+")){
+         int sum = one + two;
+         System.out.println(sum);
+      }
+      if(symbol.equals("-")){
+         System.out.println(one - two);
+      }
+      if(symbol.equals("*")){
+         System.out.println(one * two);
+      }
+      if(symbol.equals("/")){
+         if (one % two == 0){
+            System.out.println(one / two);
+         }
+         else{
+            System.out.println((double)one / (double)two);
+         }
+      }
+   }
+}
