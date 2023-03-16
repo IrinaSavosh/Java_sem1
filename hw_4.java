e178de91892a0f4876a95828930b5e868feeb6c9
IrinaSavosh <irina_savosh@mail.ru>
2023-03-16 07:05:55 +0300

    homework 4, the code is not workable, sketches

--- hw_4.java
+++ hw_4.java
@@ -1,5 +1,11 @@
 package seminar_1;
+
+import java.util.Arrays;
 import java.util.Scanner;
+// import java.lang.Character.*;
+// import java.util.Arrays;
+// import java.io.*;
+
 public class hw_4 {
    public static void main(String[] args) {
       /**(ДОПОЛНИТЕЛЬНАЯ) +Задано уравнение вида q + w = e, q, w, e >= 0.
@@ -11,8 +17,54 @@ public class hw_4 {
 
       System.out.print("Введите выражение вида q + w = e, где q, w, e >= 0: ");
       Scanner in = new Scanner(System.in);
-      String str = in.next();
+      String str = in.nextLine();
+      System.out.println(str);
+      // char elements = str.charAt(1);
+      // for(String retval : str.split(" ")){
+      String[] words = str.split(" ");
+      for (String word : words) {
+         if(word.equals("?")){
+            for (int i = 0; i < 10; i++){
+               word = Integer.toString(i);
+               System.out.println(word);
+            }
+         }
+         
+      }
+      System.out.println(Arrays.toString(words));
+      // }
+      // for (int i = 0; i < str.length(); i++) {
+      //    if(str.charAt(i).equals("?")){
+            
+      //    }
+      // }
+      in.close();
+      // for (int i = 0; i < words.length; i+=2){
+         int first_number = 0;
+         int second_number = 0;
+         System.out.print(words[0]);
+         // String first_str[] = words[0];
+         // for (int j = 0; j < first_str.length; j++) {
+         //    if (first_str[j].equals("?")){
+         //       for (int i = 0; i < 10; i++){
+             
+         //       }
+         //    }
+         // }
+         
+      }
+      // for (int i = 0; i < words.length; i++){
+      //    if (words[i] != "+" & words[i]!= "?"){
+            
+      //       count++;
 
-      
+      //    }
+      // }
+      // System.out.print((int) Math.pow((int) 7, (int) 4));
    }
-}
+
+  
+// }
+
+// {1, ?, +, ?, 5, =, 3, 0}
+
